28a8ba07ae3d117d3aeca420e87e78f43ed3e1c9
Jordan Harrell <Jordan@Jordans-MacBook-Pro.local>
2019-11-10 17:46:46 -0500

    Wrote methods for Single Customer

--- client/src/components/customer/SingleCustomer.js
+++ client/src/components/customer/SingleCustomer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 export default class SingleCustomer extends Component {
-    
+
     state = {
         updatedCustomer: {
             firstName: '',
@@ -10,25 +10,52 @@ export default class SingleCustomer extends Component {
             styleProfile: ''
         }
     }
-    
+
     componentDidMount() {
         this.refreshCustomer()
-     }
- 
-     refreshCustomer = () => {
-         const customer = this.props.match.params.customerId
-         axios.get(`/api/customer/${customer}`)
-         .then((res) => {
-             console.log('React Single Customer')
-             console.log(res.data)
-             this.setState({updatedCustomer: res.data})
-         })
-     }
+    }
+
+    refreshCustomer = () => {
+        const customer = this.props.match.params.customerId
+        axios.get(`/api/customer/${customer}`)
+            .then((res) => {
+                console.log('React Single Customer')
+                console.log(res.data)
+                this.setState({ updatedCustomer: res.data })
+            })
+    }
+
+    onUpdateCustomer = (event) => {
+        event.preventDefault()
+        const uCustomer = this.state.updatedCustomer._id
+        axios.put(`/customer/${uCustomer}`, this.state.updatedCustomer)
+    }
+
+    onNewCustomerFirstNameChange = (event) => {
+        const newCustomerFirstName = event.target.value
+        const previousState = { ...this.state }
+        previousState.updatedCustomer.firstName = newCustomerFirstName
+        this.setState(previousState)
+    }
+
+    onNewCustomerLastNameChange = (event) => {
+        const newCustomerLastName = event.target.value
+        const previousState = { ...this.state }
+        previousState.updatedCustomer.lastName = newCustomerLastName
+        this.setState(previousState)
+    }
+
+    onNewCustomerStyleProfileChange = (event) => {
+        const newCustomerStyleProfile = event.target.value
+        const previousState = { ...this.state }
+        previousState.updatedCustomer.styleProfile = newCustomerStyleProfile
+        this.setState(previousState)
+    }
 
     render() {
         return (
             <div>
-                
+
             </div>
         )
     }
