5be0522f300b16efdf5b28be3a3af45ccff9fb5c
Jordan Harrell <Jordan@Jordans-MacBook-Pro.local>
2020-01-16 18:18:47 -0500

    Put universal header in

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Dashboard from './components/dashboard/Dashboard.js'
+import Header from './components/materialLayouts/Header.js'
+import Footer from './components/materialLayouts/Footer.js'
 import AddNewCustomer from './components/customer/AddNewCustomer.js'
 import SingleCustomer from './components/customer/SingleCustomer.js'
 import AddNewInventoryItem from './components/inventory/AddNewInventoryItem.js'
@@ -12,9 +13,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Header />
       <Router>
         <Switch>
-          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/" component={Footer} />
           <Route exact path="/customer/create-customer" component={AddNewCustomer} />
           <Route exact path="/customer/:customerId" component={SingleCustomer} />
           <Route exact path="/inventory/new-piece" component={AddNewInventoryItem} />
--- client/src/components/customer/SingleCustomer.js
+++ client/src/components/customer/SingleCustomer.js
@@ -6,7 +6,6 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardActions from '@material-ui/core/CardActions'
 import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
 
 export default class SingleCustomer extends Component {
 
